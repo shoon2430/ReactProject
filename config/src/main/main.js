cef47ea7b5cc45ee8b3578077816cd7c0817e9df
unknown <shoon2430@gmail.com>
2020-08-14 10:53:53 +0900

    mobx pulgin 추가

---
+++ config/src/main/main.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class main extends Component {
+  render() {
+    return (
+      <div>
+        {/* 기능1
+                기능2
+                기능3
+                
+                */}
+      </div>
+    );
+  }
+}
+
+export default main;
